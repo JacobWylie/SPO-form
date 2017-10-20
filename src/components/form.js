d7b1a3149019c75457b324dc203afe56804d88b6
JacobWylie <jacobwylie@gmail.com>
2017-10-20 23:45:44 +0200

    renderFirstNameField(field) added with spread syntax to make available all Field properties to input

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+// { Field } interacts with redux-form. Does not render to page.
 // { reduxForm } allows component to communicate to formReducer
 import { Field, reduxForm } from 'redux-form';
 
 class Form extends Component {
+	// Returns JSX. 'field' argument by convention that contains event handlers 
+	// to connect to specific field
+	renderFirstNameField(field) {
+		return (
+			<div>
+				<input
+					type="text"
+					// all object's properties available as props to input
+					{...field.input}
+				/>
+			</div>
+		)
+	}
+
 	render() {
 		return(
-			<div>
-				Insert Form Here
-			</div>
+			<form>
+				<Field
+					firstName='firstName'
+					// Tells redux-form what to render to page.
+					component={this.renderFirstNameField}
+				/>
+			</form>
 		);
 	}
 }
 
 // reduxForm() simliar to connect()
-// 'form' = name of form
+// unique string to differentiate between forms
 export default reduxForm({
-	form: ''
+	form: 'NewAccountForm'
 })(Form);
