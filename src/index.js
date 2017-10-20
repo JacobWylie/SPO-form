aa0205424a508770131f1dd68243bdf4bbf3e54e
JacobWylie <jacobwylie@gmail.com>
2017-10-20 20:15:42 +0200

    /users path added and placeholder component rendered to page

--- src/index.js
+++ src/index.js
@@ -5,22 +5,21 @@ import { createStore, applyMiddleware } from 'redux';
 // {BroswerRouter} interacts with the history library and decides what to do 
 // based on a change in the URL.
 // {Route} will show component on page depending on URL. 
-import { BroswerRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import Users from './components/users';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-    	<BroswerRouter>
+    	<BrowserRouter>
     		{/* BrowserRouter can only have one child element... */}
     		<div>
 	    		{/* Show component based on user path/url */}
-	    		<Route path="" component={} />
-	    		<Route path="" component={} />
+	    		<Route path="/users" component={Users} />
     		</div>
-    	</BroswerRouter>
+    	</BrowserRouter>
 	</Provider>
 	, document.querySelector('.container'));
